35fea783d34384b51075961624c63cf538439bd5
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-03-04 17:00:45 +0300

    Create the 'CircleButtonAnimationView' component in the 'Components' file.

---
+++ CryptoApp/CryptoApp/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,26 @@
+//
+//  CircleAnimationView.swift
+//  CryptoApp
+//
+//  Created by Eyüphan Akkaya on 4.03.2026.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    @Binding var animate: Bool
+    
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 5.0)
+            .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0 : 1)
+            .animation(animate ? Animation.easeOut(duration: 1.0) : .none, value: animate)
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView(animate: .constant(false))
+        .foregroundStyle(.red)
+        .frame(width: 100, height: 100)
+}
