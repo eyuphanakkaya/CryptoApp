b06c107075a131e2896eff7a17188af3e0f145f1
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-03-04 16:17:17 +0300

    create 'Color+Ext'

---
+++ CryptoApp/CryptoApp/Extensions/Color+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Ext.swift
+//  CryptoApp
+//
+//  Created by Eyüphan Akkaya on 4.03.2026.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondary = Color("SecondaryTextColor")
+}
